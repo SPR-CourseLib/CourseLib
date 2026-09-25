418838b32d7c013d160edc0426110a7f39b69ce8
Горечко Владислав <brianprott456@gmail.com>
2026-09-25 20:54:51 +0300

    Sonarqube secret key and debug fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t77))bdnj*&d6&qouipfb3jv9t_^lw&o7@@el5zzq54@k&azfk'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
 
